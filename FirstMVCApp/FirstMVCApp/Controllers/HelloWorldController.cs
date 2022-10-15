fb66cdf111835f330fe39f64fc8bd04f0e020340
shalant <doug.rosenberg@gmail.com>
2022-10-14 22:01:30 -0500

    initial, 29:43

---
+++ FirstMVCApp/FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
