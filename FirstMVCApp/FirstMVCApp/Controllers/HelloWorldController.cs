4d97015e6c69f3d6c9bdef257289735d35e100ad
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-10-29 14:54:35 -0500

    finished initial mvc lesson

--- FirstMVCApp/FirstMVCApp/Controllers/HelloWorldController.cs
+++ FirstMVCApp/FirstMVCApp/Controllers/HelloWorldController.cs
@@ -5,11 +5,25 @@ namespace FirstMVCApp.Controllers
 {
     public class HelloWorldController : Controller
     {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
         public IActionResult Index()
         {
-            DogViewModel doggo = new DogViewModel()
-            { Name = "Sif", Age = 2 };
-            return View(doggo);
+            
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            //return View("Index");
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
         }
 
         public string Hello()
